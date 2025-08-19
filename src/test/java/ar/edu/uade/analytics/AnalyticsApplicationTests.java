f9190b2cd5e1eddc3d433868a34b662c6f7d3038
Sebastian Bernasconi <136746810+sebaBernasconi@users.noreply.github.com>
2025-08-18 21:28:01 -0300

    Commit inicial

---
+++ src/test/java/ar/edu/uade/analytics/AnalyticsApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.edu.uade.analytics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnalyticsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
