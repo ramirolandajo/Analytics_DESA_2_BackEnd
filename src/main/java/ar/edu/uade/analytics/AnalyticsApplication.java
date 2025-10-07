f2ed872e1fa00dd008a97a049d84cb5e209dffa3
EnzoAA004 <easplanatti@uade.edu.ar>
2025-10-07 10:24:04 -0300

    1.1.0, arreglo de fallos

--- src/main/java/ar/edu/uade/analytics/AnalyticsApplication.java
+++ src/main/java/ar/edu/uade/analytics/AnalyticsApplication.java
@@ -3,7 +3,9 @@ package ar.edu.uade.analytics;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.kafka.annotation.EnableKafka;
 
+@EnableKafka
 @SpringBootApplication
 @EntityScan(basePackages = "ar.edu.uade.analytics.Entity")
 public class AnalyticsApplication {
